26a27b71532ffe6a8453896dae884ece871a7f86
Evan Chen <echen@192.168.1.21>
2015-05-19 23:56:35 -0700

    Almost done with Quantified 2

--- config/dynamic.js
+++ config/dynamic.js
@@ -3,18 +3,42 @@ var request = require('request');
 
 function dynamic(response, parts) {
 
-    if(typeof parts[2] !== 'undefined'){
-        if(parts[2].indexOf("getKeywords") > -1){
-            var iParts = parts[2].split("?");
-            console.log(iParts);
+    if(typeof parts[2] !== 'undefined' && parts[2].indexOf("getKeywords") > -1){
+        var iParts = parts[2].split("?");
+        if(typeof iParts[1] !== 'undefined'){
+            var query = iParts[1].replace("keywords","q");
+            var url = "http://food2fork.com/api/search?key=682502870332fddb73d7c8ee7969545f&"+query;
+            request(url, function(error, res, body){
+                if(!error && response.statusCode === 200){
+                    response.writeHead(200, {"Content-Type": "application/json"});
+                    response.end(body);
+                } else {
+                    console.log(error);
+                }
+            });
         }
+    } else if(typeof parts[2] !== 'undefined' && parts[2].indexOf("getRecipe") > -1){
+        var iParts = parts[2].split("?");
+        if(typeof iParts[1] !== 'undefined'){
+            var query = iParts[1].replace("getRecipe","rId");
+            var url = "http://food2fork.com/api/get?key=682502870332fddb73d7c8ee7969545f&"+query;
+            request(url, function(error, res, body){
+                if(!error && response.statusCode === 200){
+                    response.writeHead(200, {"Content-Type": "application/json"});
+                    response.end(body);
+                } else {
+                    console.log(error);
+                }
+            });
+        }
+    } else {
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write("<h1>Hello!</h1>");
+        for(var i = 0; i < parts.length; i++){
+            response.write("<p>parts["+i+"]:"+parts[i]+"</p>");
+        }
+        response.end();
     }
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write("<h1>Hello!</h1>");
-    for(var i = 0; i < parts.length; i++){
-        response.write("<p>parts["+i+"]:"+parts[i]+"</p>");
-    }
-    response.end();
 }
 
 // make this visible when the module is required
--- public/assets/js/quantifiedRecipe.js
+++ public/assets/js/quantifiedRecipe.js
@@ -1,3 +1,17 @@
+var recipeObject;
+
+$("#keywords").keyup(function (e){
+    if(e.keyCode === 13){
+        ajaxCall();
+    }
+});
+$("body").on("click", ".recipeSelect", function(event){
+    event.preventDefault();
+    $(".recipeSelect").removeClass("btn-warning");
+    $(this).addClass("btn-warning");
+    showRecipeDetails($(this).attr("data-id"));
+});
+
 function ajaxCall(){
     // get the input
     var keywords = document.getElementById("keywords").value;
@@ -7,11 +21,46 @@ function ajaxCall(){
     // query part (?keywords=..., where ... are the keywords)
     var keywordsQuery = "/dyn/getKeywords?keywords="+keywords;
 
+    $(".recipeSelect").remove();
+
+    $("#recipeListLoader").show();
     $.ajax({
         type: 'GET',
         url: keywordsQuery,
         success: function(response){
             console.log(response);
+            recipeObject = response.recipes;
+            var i;
+
+            for(i = 0; i < response.count; i++){
+                $("#recipeList").append("<button class='btn btn-default recipeSelect' data-id='"+i+"'>"+recipeObject[i].title+"</button>");
+            }
+        },
+        complete: function(){
+            $("#recipeListLoader").hide();
+        }
+    });
+}
+function showRecipeDetails(id) {
+    console.log(id);
+    $.ajax({
+        type: 'GET',
+        url: "/dyn/getRecipe?getRecipe="+recipeObject[id].recipe_id,
+        success: function(response){
+            console.log(response);
+            var i;
+
+            $("#recipeDescrTitle").text(response.recipe.title);
+            $("#recipeDescrImage").attr("src", response.recipe.image_url);
+            $("#recipePublisher span").text(response.recipe.publisher);
+            $("#recipePublisherUrl a").attr("href", response.recipe.publisher_url);
+            $("#socialRank span").text(response.recipe.social_rank);
+            $("#recipeIngredients").empty();
+            for(i = 0; i < response.recipe.ingredients.length; i++){
+                $("#recipeIngredients").append("<li>"+response.recipe.ingredients[i]+"</li>");
+            }
+
+            $("#recipeDescrContainer").show();
         }
     });
 }
